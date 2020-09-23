75a6d8df8c45e28e15b3cc67b8d7b117564858db
mahankalpratik <mahankalpratik@gmail.com>
2020-09-23 17:35:57 +0530

    the secondary commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React, { useState } from "react";
 
 import "./App.css";
@@ -10,17 +11,31 @@ function App() {
   console.log(messages, "👉");
   const sendMessage = (event) => {
     //all the logic to seng a message goes
+    event.preventDefault();
     setMesseges([...messages, input]);
     setInput("");
   };
   return (
     <div className="App">
       <h1>Hello this is Pratik 🚀 </h1>
-      <input value={input} onChange={(event) => setInput(event.target.value)} />
-      <button onClick={sendMessage}>Send messeage</button>
-      {/* input field */}
-      {/* button  */}
-      {/* messeges themselves */}
+      <form>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <Button
+          disabled={!input}
+          variant="contained"
+          color="primary"
+          type="submit"
+          onClick={sendMessage}
+        >
+          Send messeage
+        </Button>
+      </form>
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
